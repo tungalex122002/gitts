06ea07236ea0eee3c63e39ea30cebb38253ab1a9
tungalex122002 <tungns.ba11-099@st.usth.edu.vn>
2023-09-19 11:34:53 +0700

    update code

---
+++ app/src/main/java/vn/edu/usth/tanhancut/ClothingActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.usth.tanhancut;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClothingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing);
+    }
+}
---
+++ tanhancut/app/src/main/java/vn/edu/usth/tanhancut/MainActivity.java
@@ -0,0 +1,31 @@
+package vn.edu.usth.tanhancut;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView clothingCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clothingCard = findViewById(R.id.clothingCard);
+
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ClothingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
